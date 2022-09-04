5be3057eb0deba3050b0761b41b5747c85a0165b
Brian Drelling <brian@drelling.com>
2022-09-04 00:08:58 -0500

    Added Request+Subdomain

--- Package.swift
+++ Package.swift
@@ -11,18 +11,18 @@ let package = Package(
         .library(name: "VaporExtensions", targets: ["VaporExtensions"]),
     ],
     dependencies: [
-        // 💧 Vapor
+        .package(url: "https://github.com/swift-kipple/Core", from: "0.10.2"),
         .package(url: "https://github.com/vapor/vapor", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent", from: "4.4.0"),
         // Development
-        .package(url: "https://github.com/swift-kipple/Tools", from: "0.2.5"),
+        .package(url: "https://github.com/swift-kipple/Tools", from: "0.3.1"),
     ],
     targets: [
         .target(
             name: "VaporExtensions",
             dependencies: [
-                // Vapor
                 .product(name: "Fluent", package: "fluent"),
+                .product(name: "KippleCore", package: "Core"),
                 .product(name: "Vapor", package: "vapor"),
             ]
         ),
---
+++ Sources/VaporExtensions/Core/Extensions/Request+Subdomain.swift
@@ -0,0 +1,16 @@
+// Copyright © 2022 Brian Drelling. All rights reserved.
+
+import KippleCore
+import Vapor
+
+private extension Request {
+    private static let subdomainRegex = #"([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+"#
+
+    var host: String? {
+        self.headers.first(name: .host)
+    }
+
+    var subdomain: String? {
+        self.host?.firstMatch(for: Self.subdomainRegex)
+    }
+}

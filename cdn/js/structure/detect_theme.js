3856d780236b63377ef1f5f133a1706d9d816f7f
Brian Drelling <brian@drelling.com>
2022-09-14 20:29:56 -0500

    Fixed console log comment

--- cdn/js/structure/detect_theme.js
+++ cdn/js/structure/detect_theme.js
@@ -55,7 +55,7 @@ function setThemeColor() {
     // Get the intended body background color.
     let backgroundColor = window.getComputedStyle(document.documentElement).getPropertyValue('background-color');
 
-    console.log(`Setting <meta name="theme=color"> tag content to '${backgroundColor}'.`)
+    console.log(`Setting <meta name="theme-color"> tag content to '${backgroundColor}'.`)
     
     // Update the <meta> theme-color attribute to match the theme.
     let themeColorTag = document.querySelector('head meta[name="theme-color"]')
